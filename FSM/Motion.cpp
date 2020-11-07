f8b4c69302a22302757b8d5ce4d74bb4d5eb4688
宇轩 陈 <1278472559@qq.com>
2020-11-07 10:07:40 +0800

    Update

--- FSM/Motion.cpp
+++ FSM/Motion.cpp
@@ -130,4 +130,4 @@ void GotoWashRoom::Exit(Miner* _pMiner)
 	std::cout << MINERID << "left washroom!" << std::endl;
 }
 
-GotoWashRoom::GotoWashRoom() { std::cout << "Singleton<GotoWashRoom> Create!" << std::endl; }
+GotoWashRoom::GotoWashRoom() { std::cout << "Singleton<GotoWashRoom> Createeee!" << std::endl; }
